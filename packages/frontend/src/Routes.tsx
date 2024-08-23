d06d56a036c12f9999927eebd22431fe2ecde84d
brightonmboya <b.mboya@alustudent.com>
2024-08-23 14:09:06 +0300

    added the notes component

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home.tsx";
-import NotFound from "./containers/Notfound.tsx";
+import NotFound from "./containers/NotFound.tsx";
+import Login from "./containers/Login.tsx";
+import Signup from "./containers/SignUp.tsx";
+import NewNote from "./containers/NewNote.tsx";
 
 export default function Links() {
   return (
@@ -8,6 +11,9 @@ export default function Links() {
       <Route path="/" element={<Home />} />
       {/* Finally, catch all unmatched routes */}
       <Route path="*" element={<NotFound />} />;
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
     </Routes>
   );
 }
